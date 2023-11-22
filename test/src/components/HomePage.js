e41074c014b4cdcb188438ef466597a661eedff3
Sumedh220122 <132156866+Sumedh220122@users.noreply.github.com>
2023-11-22 20:33:07 +0530

    Add files via upload

---
+++ test/src/components/HomePage.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom/cjs/react-router-dom.min';
+import 'C:/Users/ravi_/OneDrive/Desktop/Documents/Movie_DB/movie/node_modules/bootstrap/dist/css/bootstrap.min.css'
+import picture from '../images/homepage_background.jpg'
+import './HomePage.css'
+
+
+
+function HomePage() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/api/submitFormData', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Redirect to the "/movies" page after successful login
+        window.location.href = '/movie';
+      } else {
+         window.location.href = '/signup';
+      }
+    } catch (error) {
+      console.error('Error submitting login form:', error);
+    }
+  };
+
+  return (
+
+    <div className="app-container">
+      <img src={picture} alt="Movie Poster" style={{ width: '20' }} />
+      <div className="search-bar">
+        <p><strong>Enter your Details</strong></p>
+        <form  onSubmit = {handleSubmit}>
+          <input type="text" placeholder="Username"name = "username" onChange={handleChange} />
+          <input type="password" placeholder="Password" name = "password"onChange={handleChange} />
+          <button type="submit">Login</button>
+          <p><strong>Don't have an account <a href = "/signup">Sign up!</a></strong></p>
+        </form>
+      </div>
+
+    </div>
+  );
+};
+
+export default HomePage;
